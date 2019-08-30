2d08426c66949f7550ccfccdf421dbb512a5199c
soggynick432 <54693001+soggynick432@users.noreply.github.com>
2019-08-29 19:33:30 -0600

    Update Assignment 2.3.py

--- Assignment 2.3.py
+++ Assignment 2.3.py
@@ -5,30 +5,3 @@ V = 1.33 * pi * r * 3
 A = 4 * pi * r ** 2
 print(V)
 print(A)
-# chapter 3 question 2
-# d= float(input('user input diameter of a circular pizza: '))
-# p= float(input('user input price of a circular pizza: '))
-# r = d / 2
-# pi = 3.14
-# A = pi * r * 2
-# print(A)
-# PPSI = A % p / 2
-# print(PPSI)
-# # chapter 3 question 14
-# n= eval(input('how many numbers are there: '))
-# sum = 0
-# for i in range(n):
-#     x= eval(input('User input Number: '))
-#     sum = sum + x
-# average = sum / n
-# print(average)
-# # chapter 5 question 3
-# examScore= float(input('exam score: '))
-# grade = examScore / 2
-# print(grade)
-# # chapter 5 question 4
-# phrase= eval(input('user input phrase: '))
-# string = phrase
-# x = ["phrase"]
-# for i in x():
-#     i[o]
